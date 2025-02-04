f5e6c71516257ceb4f37f16361e36208a2208eb4
Desmond633 <biagiolavoro63@gmail.com>
2025-02-04 11:12:23 +0100

    Esercizi di Lunedi 04/02

--- Lunedi 04.02/Esercizio1.py
+++ Lunedi 04.02/Esercizio1.py
@@ -7,3 +7,14 @@ class Prodotto:
     
     def calcola_profitto(self):
         return self.prezzo_vendita - self.costo_produzione  # restitutzione profitto
+# classe parallela Elettronica che eredita da Prodotto
+class Elettronica(Prodotto):
+    def __init__(self, nome, costo_produzione, prezzo_vendita, garanzia):
+        super().__init__(nome, costo_produzione, prezzo_vendita)  # Iattributi della classe base
+        self.garanzia = garanzia  # nuovo attributo specifico per elettronica
+
+# classe parallela Abbigliamento che eredita da Prodotto
+class Abbigliamento(Prodotto):
+    def __init__(self, nome, costo_produzione, prezzo_vendita, materiale):
+        super().__init__(nome, costo_produzione, prezzo_vendita)  # attributi della classe base
+        self.materiale = materiale  # nuovo attributo specifico per abbigliamento
